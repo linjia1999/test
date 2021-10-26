d0dab29a67c3cb9001c4ef0834d627906c67765d
jesswhite1205 <43128453+jesswhite1205@users.noreply.github.com>
2021-10-26 09:26:02 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+api_key = '63af583a5faa849605808d646621afee'
+url = 'https://api.themoviedb.org/3/search/movie'
+parms = {
+    "api_key": api_key,
+    "query": 'ironman',
+}
+
+
+
+response = requests.get(url, params=parms)
+print(response.json())
